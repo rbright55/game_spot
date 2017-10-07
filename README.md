a173c2ec0a98a46f4cae2deb1d8f137a2bd34de7
Ryan Bright <brightryan1@gmail.com>
2017-10-07 15:36:50 -0400

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# game_spot
+# Game Spot
+* A simple website that hosts multiple games
+* Users can login through google
+* Scores are tracked for each game and posted on the score board
--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ($auth->checkRedirectCode()) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Game</title>
+	<title>Game Spot</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
